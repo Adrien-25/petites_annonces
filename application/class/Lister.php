2a74b71c4eeafeb970c405084cc432044837a2f2
Adrien SCHMIDT <adrien.schmidt7@gmail.com>
2020-09-04 08:35:00 +0200

    Ajout de la classe pour charger les annonce

--- application/class/Lister.php
+++ application/class/Lister.php
@@ -4,16 +4,26 @@ namespace App;
 class Lister
 {
     public static function appelLister(){
+        $db = new \App\Database();
+        $dataSize = $db->query("SELECT * FROM annonce");
+        // changer la variable pour l'objet connection en fonction des pages
+        $db = new \App\Database();
+        $data = $db->query("SELECT * FROM annonce LIMIT 10");
+        $dataAll = [
+            "DataLimit" => $data,
+            "DataAll" => $dataSize,
+        ];
+        return $dataAll;
+    }
+    public static function ajouterAnnonces($offset){
         // changer la variable pour l'objet connection en fonction des pages
         if (empty($offset)){
             $offset = 0;
         }
-        var_dump($offset);
         $db = new \App\Database();
         $data = $db->query("SELECT * FROM annonce LIMIT 10 OFFSET ".$offset."");
 
         $offset+=10;
-        var_dump($offset);
         return $data;
     }
 }
