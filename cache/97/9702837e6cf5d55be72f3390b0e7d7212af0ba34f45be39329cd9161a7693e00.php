5cc8012a4865bb16302750e5f4987aad71cd6d40
fouad-git <fouad.lyousfi@hotmail.com>
2020-09-07 14:38:36 +0200

    ajout cache

---
+++ cache/97/9702837e6cf5d55be72f3390b0e7d7212af0ba34f45be39329cd9161a7693e00.php
@@ -0,0 +1,165 @@
+<?php
+
+use Twig\Environment;
+use Twig\Error\LoaderError;
+use Twig\Error\RuntimeError;
+use Twig\Extension\SandboxExtension;
+use Twig\Markup;
+use Twig\Sandbox\SecurityError;
+use Twig\Sandbox\SecurityNotAllowedTagError;
+use Twig\Sandbox\SecurityNotAllowedFilterError;
+use Twig\Sandbox\SecurityNotAllowedFunctionError;
+use Twig\Source;
+use Twig\Template;
+
+/* pages/index.html.twig */
+class __TwigTemplate_e672e720880aa46cda09dac894d4a8ff6dc6957d1ccf1599d33ad2e771686a79 extends Template
+{
+    private $source;
+    private $macros = [];
+
+    public function __construct(Environment $env)
+    {
+        parent::__construct($env);
+
+        $this->source = $this->getSourceContext();
+
+        $this->blocks = [
+            'title' => [$this, 'block_title'],
+            'add' => [$this, 'block_add'],
+        ];
+    }
+
+    protected function doGetParent(array $context)
+    {
+        // line 1
+        return "./base.html.twig";
+    }
+
+    protected function doDisplay(array $context, array $blocks = [])
+    {
+        $macros = $this->macros;
+        $this->parent = $this->loadTemplate("./base.html.twig", "pages/index.html.twig", 1);
+        $this->parent->display($context, array_merge($this->blocks, $blocks));
+    }
+
+    // line 2
+    public function block_title($context, array $blocks = [])
+    {
+        $macros = $this->macros;
+        echo "listes des annonces";
+    }
+
+    // line 3
+    public function block_add($context, array $blocks = [])
+    {
+        $macros = $this->macros;
+        // line 4
+        echo "<div id=\"scroller-layout\">
+  ";
+        // line 5
+        $context['_parent'] = $context;
+        $context['_seq'] = twig_ensure_traversable(($context["listes_annonces"] ?? null));
+        foreach ($context['_seq'] as $context["_key"] => $context["annonce"]) {
+            // line 6
+            echo "  <div class=\"container text-light mb-2 scroller-item\" id=\"";
+            echo twig_escape_filter($this->env, twig_get_attribute($this->env, $this->source, $context["annonce"], "ann_id", [], "any", false, false, false, 6), "html", null, true);
+            echo "\">
+    <div class=\"border border-dark bg-dark p-1 d-flex justify-content-between\">
+      <div class=\"\">
+        <h2 class=\"\">";
+            // line 9
+            echo twig_escape_filter($this->env, twig_get_attribute($this->env, $this->source, $context["annonce"], "ann_titre", [], "any", false, false, false, 9), "html", null, true);
+            echo "</h2>
+        <p class=\"text-danger ml-2 font-weight-bold\">";
+            // line 10
+            echo twig_escape_filter($this->env, twig_get_attribute($this->env, $this->source, $context["annonce"], "ann_prix", [], "any", false, false, false, 10), "html", null, true);
+            echo " €</p>
+        <p class=\"ml-2 font-weight-bold\">";
+            // line 11
+            echo twig_escape_filter($this->env, twig_get_attribute($this->env, $this->source, $context["annonce"], "categorie_id", [], "any", false, false, false, 11), "html", null, true);
+            echo "</p>
+        <p class=\"ml-2 font-weight-bold\">Le ";
+            // line 12
+            echo twig_escape_filter($this->env, twig_date_format_filter($this->env, twig_get_attribute($this->env, $this->source, $context["annonce"], "ann_date_ecriture", [], "any", false, false, false, 12), "j m Y"), "html", null, true);
+            echo "</p>
+        <div>
+          <a href=\"\" class=\"btn bg-danger\">Détails</a>
+          <a href=\"\" class=\"btn bg-danger\">Télécharger PDF</a>
+        </div>
+      </div>
+      <img class=\"fit-picture mw-500 mh-500\" src=\"../public/assets/medias/";
+            // line 18
+            echo twig_escape_filter($this->env, twig_get_attribute($this->env, $this->source, $context["annonce"], "ann_image_nom", [], "any", false, false, false, 18), "html", null, true);
+            echo ".jpg\"
+        alt=\"";
+            // line 19
+            echo twig_escape_filter($this->env, twig_get_attribute($this->env, $this->source, $context["annonce"], "ann_image_nom", [], "any", false, false, false, 19), "html", null, true);
+            echo "\">
+    </div>
+
+  </div>
+  
+  ";
+        }
+        $_parent = $context['_parent'];
+        unset($context['_seq'], $context['_iterated'], $context['_key'], $context['annonce'], $context['_parent'], $context['loop']);
+        $context = array_intersect_key($context, $_parent) + $_parent;
+        // line 25
+        echo "<div id=\"first-loader\" class=\"ajax-loader text-center m-2\" value=";
+        echo twig_escape_filter($this->env, ($context["Nbr_annonces"] ?? null), "html", null, true);
+        echo ">
+    <img src=\"https://media2.giphy.com/media/sSgvbe1m3n93G/giphy.gif\">
+  </div>
+</div>
+";
+    }
+
+    public function getTemplateName()
+    {
+        return "pages/index.html.twig";
+    }
+
+    public function isTraitable()
+    {
+        return false;
+    }
+
+    public function getDebugInfo()
+    {
+        return array (  109 => 25,  97 => 19,  93 => 18,  84 => 12,  80 => 11,  76 => 10,  72 => 9,  65 => 6,  61 => 5,  58 => 4,  54 => 3,  47 => 2,  36 => 1,);
+    }
+
+    public function getSourceContext()
+    {
+        return new Source("{% extends \"./base.html.twig\" %}
+{% block title %}listes des annonces{% endblock title %}
+{% block add %}
+<div id=\"scroller-layout\">
+  {% for annonce in listes_annonces %}
+  <div class=\"container text-light mb-2 scroller-item\" id=\"{{annonce.ann_id}}\">
+    <div class=\"border border-dark bg-dark p-1 d-flex justify-content-between\">
+      <div class=\"\">
+        <h2 class=\"\">{{annonce.ann_titre}}</h2>
+        <p class=\"text-danger ml-2 font-weight-bold\">{{annonce.ann_prix}} €</p>
+        <p class=\"ml-2 font-weight-bold\">{{annonce.categorie_id}}</p>
+        <p class=\"ml-2 font-weight-bold\">Le {{annonce.ann_date_ecriture|date('j m Y')}}</p>
+        <div>
+          <a href=\"\" class=\"btn bg-danger\">Détails</a>
+          <a href=\"\" class=\"btn bg-danger\">Télécharger PDF</a>
+        </div>
+      </div>
+      <img class=\"fit-picture mw-500 mh-500\" src=\"../public/assets/medias/{{annonce.ann_image_nom}}.jpg\"
+        alt=\"{{annonce.ann_image_nom}}\">
+    </div>
+
+  </div>
+  
+  {% endfor %}
+<div id=\"first-loader\" class=\"ajax-loader text-center m-2\" value={{Nbr_annonces}}>
+    <img src=\"https://media2.giphy.com/media/sSgvbe1m3n93G/giphy.gif\">
+  </div>
+</div>
+{% endblock add %}", "pages/index.html.twig", "C:\\wamp64\\www\\petites-annonces\\application\\template\\pages\\index.html.twig");
+    }
+}
---
+++ cache/b5/b5a3ea4c3d6f0f21f708ca93d26d0fe9b317cc1ce371477691f83a3035383807.php
@@ -0,0 +1,244 @@
+<?php
+
+use Twig\Environment;
+use Twig\Error\LoaderError;
+use Twig\Error\RuntimeError;
+use Twig\Extension\SandboxExtension;
+use Twig\Markup;
+use Twig\Sandbox\SecurityError;
+use Twig\Sandbox\SecurityNotAllowedTagError;
+use Twig\Sandbox\SecurityNotAllowedFilterError;
+use Twig\Sandbox\SecurityNotAllowedFunctionError;
+use Twig\Source;
+use Twig\Template;
+
+/* ./base.html.twig */
+class __TwigTemplate_9b5c2da669e66b2b8f1c27c0b08d4f6f2e81edcf918ef5eb6c4d4e5f52c05d69 extends Template
+{
+    private $source;
+    private $macros = [];
+
+    public function __construct(Environment $env)
+    {
+        parent::__construct($env);
+
+        $this->source = $this->getSourceContext();
+
+        $this->parent = false;
+
+        $this->blocks = [
+            'head' => [$this, 'block_head'],
+            'title' => [$this, 'block_title'],
+            'add' => [$this, 'block_add'],
+            'javascript' => [$this, 'block_javascript'],
+        ];
+    }
+
+    protected function doDisplay(array $context, array $blocks = [])
+    {
+        $macros = $this->macros;
+        // line 1
+        echo "<!DOCTYPE html>
+<html lang=\"fr\">
+
+<head>
+    ";
+        // line 5
+        $this->displayBlock('head', $context, $blocks);
+        // line 22
+        echo "</head>
+
+<body>
+    <main>
+        <!--Barre de navigation-->
+        <nav class=\"navbar navbar-dark bg-dark justify-content-between mb-5\">
+            <div class=\"container\">
+                <a class=\"navbar-brand\" href=\"#\">LamaShop</a>
+                <div class=\"d-flex\">
+                    <!-- Formulaire de recherche -->
+                    <form class=\"form-inline mr-4\">
+                        <select class=\"custom-select rounded-0 bg-danger text-light border-0 font-weight-bold\"
+                            id=\"SearchForm\">
+                            <option selected>Catégories</option>
+                            <option value=\"Immobilier\">Immobilier</option>
+                        </select>
+                        <input class=\"form-control rounded-0 border-0\" type=\"search\" placeholder=\"Une petite envie ?\"
+                            aria-label=\"Search\">
+                        <button class=\"btn rounded-0 rounded-right d-flex bg-danger\" type=\"submit\"><i
+                                class=\"material-icons text-light\">search</i></button>
+                    </form>
+                    <!-- Lien vers page poster -->
+                    <a href=\"#\" class=\"btn btn-danger d-flex p-2\" type=\"button\">
+                        <span class=\"d-flex mr-2\">
+                            <i class=\"material-icons\">add_circle_outline</i>
+                        </span>
+                        <span class=\"font-weight-bold\">Poster une annonce</span>
+                    </a>
+                </div>
+            </div>
+        </nav>
+        ";
+        // line 53
+        $this->displayBlock('add', $context, $blocks);
+        // line 56
+        echo "    </main>
+    <footer>
+    </footer>
+    ";
+        // line 59
+        $this->displayBlock('javascript', $context, $blocks);
+        // line 71
+        echo "
+</body>
+
+</html>";
+    }
+
+    // line 5
+    public function block_head($context, array $blocks = [])
+    {
+        $macros = $this->macros;
+        // line 6
+        echo "    <meta charset=\"UTF-8\">
+    <!--Faire savoir au navigateur que le site Web est optimisé pour les mobiles-->
+    <meta name=\"viewport\" content=\"width=device-width, initial-scale=1, shrink-to-fit=no\">
+    <!--Importer la police d'icônes Google-->
+    <link href=\"https://fonts.googleapis.com/icon?family=Material+Icons\" rel=\"stylesheet\">
+    <!--Lien page de style-->
+    <link href=\"css/style.css\" rel=\"stylesheet\">
+    <!--Lien CDN CSS Bootstrap-->
+    <link rel=\"stylesheet\" href=\"https://stackpath.bootstrapcdn.com/bootstrap/4.2.1/css/bootstrap.min.css\"
+        integrity=\"sha384-GJzZqFGwb1QTTN6wy59ffF1BuGJpLSa9DkKMp0DgiMDm4iYMj70gZWKYbI706tWS\" crossorigin=\"anonymous\">
+    ";
+        // line 17
+        echo "    <script src=\"https://ajax.googleapis.com/ajax/libs/jquery/3.5.1/jquery.min.js\"></script>
+
+    <!--Titre-->
+    <title>";
+        // line 20
+        $this->displayBlock('title', $context, $blocks);
+        echo " </title>
+    ";
+    }
+
+    public function block_title($context, array $blocks = [])
+    {
+        $macros = $this->macros;
+        echo "Base Twig";
+    }
+
+    // line 53
+    public function block_add($context, array $blocks = [])
+    {
+        $macros = $this->macros;
+        // line 54
+        echo "
+        ";
+    }
+
+    // line 59
+    public function block_javascript($context, array $blocks = [])
+    {
+        $macros = $this->macros;
+        // line 60
+        echo "    <!--Js-->
+    <script src=\"../public/js/app.js\"></script>
+    <!-- jQuery first, then Popper.js, then Bootstrap JS -->
+    <script src=\"https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.14.6/umd/popper.min.js\"
+        integrity=\"sha384-wHAiFfRlMFy6i5SRaxvfOCifBUQy1xHdJ/yoi7FRNXMRBu5WHdZYu1hA6ZOblgut\" crossorigin=\"anonymous\">
+    </script>
+    <script src=\"https://stackpath.bootstrapcdn.com/bootstrap/4.2.1/js/bootstrap.min.js\"
+        integrity=\"sha384-B0UglyR+jN6CkvvICOB2joaf5I4l3gm9GU6Hc1og6Ls7i6U/mkkaduKaBhlAXv9k\" crossorigin=\"anonymous\">
+    </script>
+
+    ";
+    }
+
+    public function getTemplateName()
+    {
+        return "./base.html.twig";
+    }
+
+    public function getDebugInfo()
+    {
+        return array (  144 => 60,  140 => 59,  135 => 54,  131 => 53,  119 => 20,  114 => 17,  102 => 6,  98 => 5,  91 => 71,  89 => 59,  84 => 56,  82 => 53,  49 => 22,  47 => 5,  41 => 1,);
+    }
+
+    public function getSourceContext()
+    {
+        return new Source("<!DOCTYPE html>
+<html lang=\"fr\">
+
+<head>
+    {% block head %}
+    <meta charset=\"UTF-8\">
+    <!--Faire savoir au navigateur que le site Web est optimisé pour les mobiles-->
+    <meta name=\"viewport\" content=\"width=device-width, initial-scale=1, shrink-to-fit=no\">
+    <!--Importer la police d'icônes Google-->
+    <link href=\"https://fonts.googleapis.com/icon?family=Material+Icons\" rel=\"stylesheet\">
+    <!--Lien page de style-->
+    <link href=\"css/style.css\" rel=\"stylesheet\">
+    <!--Lien CDN CSS Bootstrap-->
+    <link rel=\"stylesheet\" href=\"https://stackpath.bootstrapcdn.com/bootstrap/4.2.1/css/bootstrap.min.css\"
+        integrity=\"sha384-GJzZqFGwb1QTTN6wy59ffF1BuGJpLSa9DkKMp0DgiMDm4iYMj70gZWKYbI706tWS\" crossorigin=\"anonymous\">
+    {# CDN jQuery #}
+    <script src=\"https://ajax.googleapis.com/ajax/libs/jquery/3.5.1/jquery.min.js\"></script>
+
+    <!--Titre-->
+    <title>{% block title %}Base Twig{% endblock title %} </title>
+    {% endblock head %}
+</head>
+
+<body>
+    <main>
+        <!--Barre de navigation-->
+        <nav class=\"navbar navbar-dark bg-dark justify-content-between mb-5\">
+            <div class=\"container\">
+                <a class=\"navbar-brand\" href=\"#\">LamaShop</a>
+                <div class=\"d-flex\">
+                    <!-- Formulaire de recherche -->
+                    <form class=\"form-inline mr-4\">
+                        <select class=\"custom-select rounded-0 bg-danger text-light border-0 font-weight-bold\"
+                            id=\"SearchForm\">
+                            <option selected>Catégories</option>
+                            <option value=\"Immobilier\">Immobilier</option>
+                        </select>
+                        <input class=\"form-control rounded-0 border-0\" type=\"search\" placeholder=\"Une petite envie ?\"
+                            aria-label=\"Search\">
+                        <button class=\"btn rounded-0 rounded-right d-flex bg-danger\" type=\"submit\"><i
+                                class=\"material-icons text-light\">search</i></button>
+                    </form>
+                    <!-- Lien vers page poster -->
+                    <a href=\"#\" class=\"btn btn-danger d-flex p-2\" type=\"button\">
+                        <span class=\"d-flex mr-2\">
+                            <i class=\"material-icons\">add_circle_outline</i>
+                        </span>
+                        <span class=\"font-weight-bold\">Poster une annonce</span>
+                    </a>
+                </div>
+            </div>
+        </nav>
+        {% block add %}
+
+        {% endblock add %}
+    </main>
+    <footer>
+    </footer>
+    {% block javascript %}
+    <!--Js-->
+    <script src=\"../public/js/app.js\"></script>
+    <!-- jQuery first, then Popper.js, then Bootstrap JS -->
+    <script src=\"https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.14.6/umd/popper.min.js\"
+        integrity=\"sha384-wHAiFfRlMFy6i5SRaxvfOCifBUQy1xHdJ/yoi7FRNXMRBu5WHdZYu1hA6ZOblgut\" crossorigin=\"anonymous\">
+    </script>
+    <script src=\"https://stackpath.bootstrapcdn.com/bootstrap/4.2.1/js/bootstrap.min.js\"
+        integrity=\"sha384-B0UglyR+jN6CkvvICOB2joaf5I4l3gm9GU6Hc1og6Ls7i6U/mkkaduKaBhlAXv9k\" crossorigin=\"anonymous\">
+    </script>
+
+    {% endblock javascript %}
+
+</body>
+
+</html>", "./base.html.twig", "C:\\wamp64\\www\\petites-annonces\\application\\template\\base.html.twig");
+    }
+}
