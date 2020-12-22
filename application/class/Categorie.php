23c43e88b2005b5d1ea5c8d704cea16eea70f7df
Adrien SCHMIDT <adrien.schmidt7@gmail.com>
2020-12-22 21:28:13 +0100

    Création d'une nouvelle fonction pour récupérer les catégories lors de la recerches

--- application/class/Categorie.php
+++ application/class/Categorie.php
@@ -14,5 +14,13 @@ class Categorie
         $this->dbh->execReq();
         return $this->dbh->getAll();
     }
+    public function selectionCategorieById($id)
+    {
+        $this->sql = 'SELECT id, cat_libelle FROM categorie';
+        $this->dbh = new \App\Database();
+        $this->dbh->prepareSql($this->sql);
+        $this->dbh->execReq();
+        return $this->dbh->getAll();
+    }
 }
 ?>
