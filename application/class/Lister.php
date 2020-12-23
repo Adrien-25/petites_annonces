2a04e8e3631b8061c60a0abefbf29e590f17ca22
Adrien SCHMIDT <adrien.schmidt7@gmail.com>
2020-12-23 12:32:11 +0100

    Ajout de la requête si un input est inséré et pas de catégorie sélectionnée

--- application/class/Lister.php
+++ application/class/Lister.php
@@ -9,7 +9,7 @@ class Lister{
         $dataSize = $db->query("SELECT * FROM annonce");
         // changer la variable pour l'objet connection en fonction des pages
         $db = new \App\Database();
-        $data = $db->query('SELECT `ann_id`,`ann_description`,`ann_titre`,`ann_prix`,`ann_date_ecriture`,`ann_image_url`,`ann_image_nom`,`cat_libelle`, `email` FROM annonce INNER JOIN categorie ON annonce.categorie_id = categorie.id  INNER JOIN utilisateur ON annonce.utilisateur_id = utilisateur.id  WHERE ann_est_valider = 0 LIMIT 10'); 
+        $data = $db->query('SELECT ann_id,ann_description,ann_titre,ann_prix,ann_date_ecriture,ann_image_url,ann_image_nom,cat_libelle, email FROM annonce INNER JOIN categorie ON annonce.categorie_id = categorie.id  INNER JOIN utilisateur ON annonce.utilisateur_id = utilisateur.id  WHERE ann_est_valider = 0 LIMIT 10'); 
     
         $dataAll = [
             "DataLimit" => $data,
@@ -17,9 +17,23 @@ class Lister{
         ];
         return $dataAll;
     }
-    public static function appelListerSearch(){
-        $db = new \App\Database();
-        $dataSize = $db->query("SELECT * FROM annonce");
+    public static function appelListerSearch($formData){
+        if ($formData['searchInput'] && count($formData) == 1){
+            $input = $formData['searchInput'];
+            $db = new \App\Database();
+            $dataSize = $db->query("SELECT * FROM annonce WHERE ann_description LIKE '%".$input."%'");
+            $db = new \App\Database();
+            $data = $db->query("SELECT ann_id,ann_description,ann_titre,ann_prix,ann_date_ecriture,ann_image_url,ann_image_nom,cat_libelle, email FROM annonce INNER JOIN categorie ON annonce.categorie_id = categorie.id  INNER JOIN utilisateur ON annonce.utilisateur_id = utilisateur.id  WHERE ann_est_valider = 0 AND ann_description LIKE '%".$input."%' LIMIT 10"); 
+        } 
+
+        // $db = new \App\Database();
+        // $data = $db->query('SELECT ann_id,ann_description,ann_titre,ann_prix,ann_date_ecriture,ann_image_url,ann_image_nom,cat_libelle, email FROM annonce INNER JOIN categorie ON annonce.categorie_id = categorie.id  INNER JOIN utilisateur ON annonce.utilisateur_id = utilisateur.id  WHERE ann_est_valider = 0 LIMIT 10'); 
+
+        $dataAll = [
+            "DataLimit" => $data,
+            "DataAll" => $dataSize,
+        ];
+        return $dataAll;
 
     }
     public static function ajouterAnnonces($offset){
@@ -28,7 +42,7 @@ class Lister{
             $offset = 0;
         }
         $db = new \App\Database();
-        $data = $db->query("SELECT `ann_id`,`ann_description`,`ann_titre`,`ann_prix`,`ann_date_ecriture`,`ann_image_url`,`ann_image_nom`,`cat_libelle`, `email` FROM annonce INNER JOIN categorie ON annonce.categorie_id = categorie.id  INNER JOIN utilisateur ON annonce.utilisateur_id = utilisateur.id  WHERE ann_est_valider = 0 LIMIT 10 OFFSET ".$offset."");
+        $data = $db->query("SELECT ann_id,ann_description,ann_titre,ann_prix,ann_date_ecriture,ann_image_url,ann_image_nom,cat_libelle, email FROM annonce INNER JOIN categorie ON annonce.categorie_id = categorie.id  INNER JOIN utilisateur ON annonce.utilisateur_id = utilisateur.id  WHERE ann_est_valider = 0 LIMIT 10 OFFSET ".$offset."");
         // $offset+=10;
         return $data;
     }
