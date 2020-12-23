79cfcdc1180d04ed6e4640ad10b4d54f364826c8
Adrien SCHMIDT <adrien.schmidt7@gmail.com>
2020-12-23 12:32:35 +0100

    Suppression de la fonction pour récupérer les catégie

--- application/class/Categorie.php
+++ application/class/Categorie.php
@@ -14,13 +14,5 @@ class Categorie
         $this->dbh->execReq();
         return $this->dbh->getAll();
     }
-    public function selectionCategorieById($id)
-    {
-        $this->sql = 'SELECT id, cat_libelle FROM categorie';
-        $this->dbh = new \App\Database();
-        $this->dbh->prepareSql($this->sql);
-        $this->dbh->execReq();
-        return $this->dbh->getAll();
-    }
 }
 ?>
